fc381b01254510a1c7174179d005b6b96b38a144
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-03-05 11:49:43 -0700

    Add cafe model spec

---
+++ cafes/spec/models/cafe_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Cafe do
+  describe "Validations" do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :street_address}
+    it {should validate_presence_of :post_code}
+    it {should validate_presence_of :num_of_chairs}
+  end
+end
