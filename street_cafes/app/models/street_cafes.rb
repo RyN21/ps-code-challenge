a7f365fc3f4ca47e112fdcc3e7e6f0a517d8ab82
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-03-05 11:36:04 -0700

    Add new rails app cafes

--- street_cafes/app/models/street_cafes.rb
+++
@@ -1,6 +0,0 @@
-class StreetCafes < ApplicationRecord
-  validates_presence_of :name,
-                        :street_address,
-                        :post_code,
-                        :num_of_chairs
-end
--- street_cafes/db/migrate/20210305030610_add_category_to_street_cafes.rb
+++
@@ -1,5 +0,0 @@
-class AddCategoryToStreetCafes < ActiveRecord::Migration[6.0]
-  def change
-    add_column :street_cafes, :category, :string
-  end
-end
