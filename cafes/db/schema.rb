5e98ca1c53fe7371f3706d6828fb49efcff087e6
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-03-05 12:19:08 -0700

    Add category to table

--- cafes/db/schema.rb
+++ cafes/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_185103) do
+ActiveRecord::Schema.define(version: 2021_03_05_191827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_03_05_185103) do
     t.string "num_of_chairs"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
   end
 
 end
