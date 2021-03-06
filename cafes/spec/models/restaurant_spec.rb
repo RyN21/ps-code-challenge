0f84844f042753f26fd68eedfac0275ec69fc8fa
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-03-05 21:01:15 -0700

    by_post_code method almost complete. Still trying to figure out how to get the name of the cafe with the max chais in the specific post_code

--- cafes/spec/models/restaurant_spec.rb
+++ cafes/spec/models/restaurant_spec.rb
@@ -9,35 +9,35 @@ RSpec.describe Restaurant, type: :model do
   end
   describe "class methods" do
     before(:each) do
-      @restaurant_1  = Restaurant.create(name: "All Bar One",
+      @restaurant_1 = Restaurant.create(name: "All Bar One",
                                         street_address: "27 East Parade",
                                         post_code: "LS1 5BN",
                                         num_of_chairs: 20)
-      @restaurant_2  = Restaurant.create(name: "All Bar One",
+      @restaurant_2 = Restaurant.create(name: "All Bar One",
                                         street_address: "Unit D Electric Press, 4 Mi",
                                         post_code: "LS2 3AD",
                                         num_of_chairs: 140)
-      @restaurant_3  = Restaurant.create(name: "Bagel Nash",
+      @restaurant_3 = Restaurant.create(name: "Bagel Nash",
                                         street_address: "34 St. Pauls Street",
                                         post_code: "LS1 2AT",
                                         num_of_chairs: 14)
-      @restaurant_4  = Restaurant.create(name: "Caffé Nero (Albion Place side)",
+      @restaurant_4 = Restaurant.create(name: "Caffé Nero (APS)",
                                         street_address: "19 Albion Place",
                                         post_code: "LS1 6JS",
                                         num_of_chairs: 20)
-      @restaurant_5  = Restaurant.create(name: "Caffe Nero (Albion Street s",
+      @restaurant_5 = Restaurant.create(name: "Caffe Nero (Albion Street s",
                                         street_address: "19 Albion Place",
                                         post_code: "LS1 6JS",
                                         num_of_chairs: 16)
-      @restaurant_6  = Restaurant.create(name: "Caffe Nero (Bond Street side) ",
+      @restaurant_6 = Restaurant.create(name: "Caffe Nero (Bond Street side)",
                                         street_address: "19 Albion Place",
                                         post_code: "LS1 6JS",
                                         num_of_chairs: 22)
-      @restaurant_7  = Restaurant.create(name: "Carluccios",
+      @restaurant_7 = Restaurant.create(name: "Carluccios",
                                         street_address: "5 Greek Street",
                                         post_code: "LS1 5SX",
                                         num_of_chairs: 18)
-      @restaurant_8  = Restaurant.create(name: "Cattle Grid",
+      @restaurant_8 = Restaurant.create(name: "Cattle Grid",
                                         street_address: "Waterloo House, Assembly St",
                                         post_code: "LS2 7DB",
                                         num_of_chairs: 20)
@@ -45,17 +45,20 @@ RSpec.describe Restaurant, type: :model do
 
     it '.can sort restaurants by post code' do
       data = Restaurant.by_post_code
-      expect(data[0].post_code).to eq("LS1 2AT")
-      expect(data[0].total_places).to eq(1)
-      expect(data[0].total_chairs).to eq(14)
-      # expect(data[0].chairs_pct).to eq("LS1 2AT")
-      expect(data[0].place_with_max_chairs).to eq("Bagel Nash")
+      binding.pry
+      expect(data[0]["post_code"]).to eq("LS1 2AT")
+      expect(data[0]["total_places"]).to eq(1)
+      expect(data[0]["total_chairs"]).to eq(14)
+      expect(data[0]["chairs_pct"]).to eq("5.19")
+      # expect(data[0].place_with_max_chairs).to eq("Bagel Nash")
+      expect(data[0]["max_chairs"]).to eq(14)
 
-      expect(data[20].post_code).to eq("LS2 3AD")
-      expect(data[20].total_places).to eq(1)
-      expect(data[20].total_chairs).to eq(140)
-      # expect(data[20].chairs_pct).to eq("LS1 2AT")
-      expect(data[20].place_with_max_chairs).to eq("All Bar One")
+      expect(data[3]["post_code"]).to eq("LS1 6JS")
+      expect(data[3]["total_places"]).to eq(3)
+      expect(data[3]["total_chairs"]).to eq(58)
+      expect(data[3]["chairs_pct"]).to eq("21.48")
+      # expect(data[3].place_with_max_chairs).to eq("Caffe Nero (Bond Street side)")
+      expect(data[3]["max_chairs"]).to eq(22)
     end
   end
 end
