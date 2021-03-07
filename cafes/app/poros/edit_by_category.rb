548128064f348ec17ff66eaf5d848d35c5565407
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-03-07 10:51:36 -0700

    Create method that export data to csv and deletes records. Tests pass

--- cafes/app/poros/edit_by_category.rb
+++ cafes/app/poros/edit_by_category.rb
@@ -1,1 +1,33 @@
+class EditByCategory
+  attr_reader :restaurants
 
+  def initialize(restaurants)
+    @restaurants = restaurants
+  end
+
+  def export_data_to_csv(data)
+    headers = ["Café/Restaurant Name", "Street Address", "Post Code", "Number Of Chairs", "Category"]
+    attributes = ["name", "street_address", "post_code", "num_of_chairs", "category"]
+
+    CSV.generate(headers: true) do |csv|
+      csv << headers
+
+      data.each do |r|
+        csv << attributes.map do |attr|
+          r.send(attr)
+        end
+      end
+    end
+  end
+
+  def delete_records(data)
+    data.each do |r|
+      r.delete
+    end
+  end
+
+  def find_small
+    @restaurants.where("category LIKE ?", "%small")
+  end
+
+end
--- cafes/spec/poros/edit_by_category_test.rb
+++ cafes/spec/poros/edit_by_category_test.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'csv'
 
 RSpec.describe EditByCategory, type: :model do
   before(:each) do
@@ -14,38 +15,45 @@ RSpec.describe EditByCategory, type: :model do
     Restaurant.categorize_restaurants
   end
 
-  it 'can delete restaurants categorized as small' do
-    small_restaurants = EditByCategory.new(Restaurant.where("category LIKE ?", "%small"))
-    small_restaurants.export_data_to_csv
+  it 'can create csv for small restaurants' do
+    restaurants = EditByCategory.new(Restaurant.all)
+    csv_small = restaurants.export_data_to_csv(restaurants.find_small)
+    csv = CSV.parse(csv_small)
 
-    csv = restaurants.to_csv
-    new__small_restaurants = CSV.foreach(small_restaurants.export_data_to_csv, headers: true) do |row|
-      Restaurant.create({
-        name: row[0],
-        street_address: row[1],
-        post_code: row[2],
-        num_of_chairs: row[3],
-        category: row[4]
-        })
-    end
-    expect(new__small_restaurants[0].category).to eq("ls1 medium")
+    # Check Headers
+    expect(csv[0][0]).to eq("Café/Restaurant Name")
+    expect(csv[0][1]).to eq("Street Address")
+    expect(csv[0][2]).to eq("Post Code")
+    expect(csv[0][3]).to eq("Number Of Chairs")
+    expect(csv[0][4]).to eq("Category")
 
-    expect(Restaurant.where("category LIKE ?", "%small").count).to eq(3)
+    # Check first row of csv file
+    expect(csv[1][0]).to eq("Barburrito")
+    expect(csv[1][1]).to eq("62 The Headrow")
+    expect(csv[1][2]).to eq("LS1 8EQ")
+    expect(csv[1][3]).to eq("8")
+    expect(csv[1][4]).to eq("ls1 small")
 
-    delete_records(small_restaurants)
-    expect(Restaurant.where("category LIKE ?", "%small").count).to eq(0)
+    # Check the correct count (This include header row)
+    expect(csv.count).to eq(4)
   end
 
+  it 'can delete record of small restaurants' do
+    restaurants = EditByCategory.new(Restaurant.all)
+    small = restaurants.find_small
+    expect(small.count).to eq(3)
+
+    restaurants.delete_records(small)
+    new_small = restaurants.find_small
+    expect(new_small.count).to eq(0)
+  end
+
+  # it 'can edit names from csv and return edited csv' do
+  #   restaurants = EditByCategory.new(Restaurant.all)
+  #   binding.pry
+  #   # expect(restaurants[0].category).to eq("ls1 medium")
+  #   # expect(restaurants[1].category).to eq("ls2 large")
+  #   # expect(restaurants[4].category).to eq("ls1 small")
+  #   # expect(restaurants[17].category).to eq("ls2 small")
+  # end
 end
-
-
-
-
-# it 'can edit names from csv and return edited csv' do
-#   restaurants = EditByCategory.new(Restaurant.all)
-#   binding.pry
-#   # expect(restaurants[0].category).to eq("ls1 medium")
-#   # expect(restaurants[1].category).to eq("ls2 large")
-#   # expect(restaurants[4].category).to eq("ls1 small")
-#   # expect(restaurants[17].category).to eq("ls2 small")
-# end
